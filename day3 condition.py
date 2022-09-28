c6c3613b140921a9c0df6f3406ff47aac8c7f9a1
changhunJung <changhun611@gmail.com>
2022-09-28 22:50:03 +0900

    conditional flow

---
+++ day3 condition.py
@@ -0,0 +1,17 @@
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is you height in cm?"))
+#
+# if height > 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry, you have to grow taller before you can ride")
+
+# 2 even or odd
+
+number = int(input("Which number do you want to check?"))
+
+if (number%2 == 0):
+    print("This is an even number")
+else:
+    print("This is an odd number")
