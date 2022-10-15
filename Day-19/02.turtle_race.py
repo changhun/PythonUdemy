4715877033224961a4021ed54e3f681156ee70ae
changhunJung <changhun611@gmail.com>
2022-10-15 09:00:39 +0900

    turtle race ver.2

--- Day-19/02.turtle_race.py
+++ Day-19/02.turtle_race.py
@@ -3,8 +3,12 @@ import random
 
 
 # tim = t.Turtle()
+width = 1000
+height = 400
+
 screen = t.Screen()
-screen.setup(width=500, height=400)
+#screen.setup(width=500, height=400)
+screen.setup(width=width, height=height)
 
 user_bat = screen.textinput(title = "Turtle Game", prompt = "Which turtle win the race? Type color: ")
 print(user_bat)
@@ -23,33 +27,53 @@ def create_rainbow_turtles():
         new_turtle.color(color)
         new_turtle.penup()
         new_turtles.append(new_turtle)
+
     return new_turtles
 
 
 def move_turtles_to_starting(_turtles):
-    for i in range(len(_turtles)):
-        _turtles[i].goto(-200, -100 + i * 30)
+    for i in range(len(_turtles) - 1,  -1, -1):
+        _turtles[i].goto(-screen.window_width()/2 + 100, -100 + i * 30)
 
 
-def is_arrived(turtle):
-    return not (turtle.xcor() < 160)
+def is_arrived(turtle, end_line):
+    return not (turtle.xcor() < end_line)
 
 
 def is_race_end(_turtles):
+    end_line = screen.window_width()/2 - 100
     for turtle in _turtles:
-        if is_arrived(turtle):
+        if is_arrived(turtle, end_line):
+            return True
+    return False
+
+
+def race_almost_end(_turtles, end_line):
+    for turtle in _turtles:
+        if turtle.xcor() >= end_line - 100:
             return True
     return False
 
 
 def random_move(turtle):
-    turtle.forward(random.choice(range(1, 10)))
+    turtle.forward(random.randint(0, 10))
+
+
+def random_slow_move(turtle):
+    random_distance = random.random() * 2
+    turtle.forward(random_distance)
+    #print(f"distance: {random_distance}")
 
 
 def start_race(_turtles):
-    while not is_race_end(_turtles):
+    end_line = screen.window_width()/2 - 100
+    while not race_almost_end(_turtles, end_line):
         for turtle in _turtles:
             random_move(turtle)
+
+    while not is_race_end(_turtles):
+        for turtle in _turtles:
+            random_slow_move(turtle)
 
 
 def print_winner(_turtles):
@@ -60,6 +84,18 @@ def print_winner(_turtles):
     print(f"winner is {colors[winner]}")
 
 
+def line_drawing():
+    turtle = t.Turtle()
+    turtle.shape("turtle")
+    turtle.width(5)
+    turtle.penup()
+    turtle.goto(screen.window_width()/2 - 100, screen.window_height()/2 - 50)
+    turtle.pendown()
+    turtle.setheading(-90)
+    turtle.forward(screen.window_height() - 100)
+
+
+line_drawing()
 turtles = create_rainbow_turtles()
 move_turtles_to_starting(turtles)
 start_race(turtles)
