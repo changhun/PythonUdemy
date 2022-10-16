262e25d835bcbbaa91408ec4d308d739d6f4cdf3
changhunJung <changhun611@gmail.com>
2022-10-16 19:00:46 +0900

    Pong GAME speed up incrementally.

--- Day22/ball.py
+++ Day22/ball.py
@@ -11,21 +11,27 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.setheading(-40)
+        self.ball_speed = 0.05
 
     def move(self):
         self.forward(MOVE_DISTANCE)
         # print("Ball moving")
 
-    def bounce(self):
+    def bounce_y(self):
         self.setheading(-self.heading())
 
-    def bounce_collision(self):
+    def bounce_x(self):
         heading = self.heading()
-        print(f"Collision with paddle. heading: {heading}")
-        print(f"xcor: {self.xcor()} ycor: {self.ycor()}")
+        # print(f"Collision with paddle. heading: {heading}")
+        # print(f"xcor: {self.xcor()} ycor: {self.ycor()}")
         if heading > 0:
             heading = 180 - heading
         else:
             heading = -180 - heading
         self.setheading(heading)
+        self.ball_speed *= 0.8
 
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.ball_speed = 0.05
--- Day22/main.py
+++ Day22/main.py
@@ -8,6 +8,13 @@ import time
 INITIAL_POSITION = [(-390, 0), (380, 0)]
 SCOREBOARD_POSITIONS = [(-250, 270), (150, 270)]
 
+
+def reset_screen(_screen):
+    _screen.clearscreen()
+    _screen.bgcolor("black")
+    _screen.tracer(0)
+
+
 screen = t.Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
@@ -15,55 +22,40 @@ screen.title("Pong")
 screen.tracer(0)
 screen.listen()
 
-
-def reset_screen(_screen):
-    screen.clearscreen()
-    screen.bgcolor("black")
-    screen.tracer(0)
-
-
 scoreBoard1 = ScoreBoard(SCOREBOARD_POSITIONS[0])
 scoreBoard2 = ScoreBoard(SCOREBOARD_POSITIONS[1])
 
+pad1 = Pad(INITIAL_POSITION[0])
+pad2 = Pad(INITIAL_POSITION[1])
+screen.onkey(pad1.up, "w")
+screen.onkey(pad1.down, "s")
+screen.onkey(pad2.up, "Up")
+screen.onkey(pad2.down, "Down")
 
-def start_game():
-    pad1 = Pad(INITIAL_POSITION[0])
-    pad2 = Pad(INITIAL_POSITION[1])
-    screen.onkey(pad1.up, "w")
-    screen.onkey(pad1.down, "s")
-    screen.onkey(pad2.up, "Up")
-    screen.onkey(pad2.down, "Down")
+ball = Ball()
+end_of_game = False
+while not end_of_game:
+    ball.move()
+    # bounce when upper or lower screen collision
+    if ball.ycor() >= 280 or ball.ycor() <= -280:
+        ball.bounce_y()
+    # bounce when the collision with paddle
+    if ball.distance(pad1) < 50 and ball.xcor() <= -370:
+        ball.bounce_x()
+    elif ball.distance(pad2) < 50 and ball.xcor() >= 360:
+        ball.bounce_x()
 
-    ball = Ball()
-    end_of_game = False
-    while not end_of_game:
-        ball.move()
-        # bounce when upper or lower screen collision
-        if ball.ycor() >= 280 or ball.ycor() <= -280:
-            ball.bounce()
-        # bounce when the collision with paddle
-        if ball.distance(pad1) < 50 and ball.xcor() <= -370:
-            ball.bounce_collision()
-        elif ball.distance(pad2) < 50 and ball.xcor() >= 360:
-            ball.bounce_collision()
+    # Detect collision with side wall
+    if ball.xcor() >= 380:
+        scoreBoard1.score_up()
+        ball.reset_position()
 
-        # Detect collision with side wall
-        if ball.xcor() >= 380:
-            scoreBoard1.score_up()
-            end_of_game = True
-        elif ball.xcor() <= -390:
+    elif ball.xcor() <= -390:
+        scoreBoard2.score_up()
+        ball.reset_position()
 
-            scoreBoard2.score_up()
-            end_of_game = True
+    screen.update()
+    time.sleep(ball.ball_speed)
 
-        screen.update()
-        time.sleep(0.05)
-
-
-while True:
-    start_game()
-    reset_screen(screen)
-    scoreBoard1.show()
-    scoreBoard2.show()
 
 screen.exitonclick()
