cc9e635b0d45a790a598adceb08a114a850893dd
changhunJung <changhun611@gmail.com>
2022-10-18 23:31:37 +0900

    Learned about file. open, read, write, with open as

---
+++ Day24/file_example.py
@@ -0,0 +1,15 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("my_file.txt", mode="w") as file:
+    file.write("new text")
+
+with open("my_file.txt", mode="a") as file:
+    file.write("\nadded text")
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
