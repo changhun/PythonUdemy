ba572101a4fd7c6822384de20bccd30398afa810
changhunJung <changhun611@gmail.com>
2022-10-25 00:20:58 +0900

    Learned about tkinter's components(Label, Button, input)

--- Day27/main.py
+++ Day27/main.py
@@ -8,5 +8,22 @@ window.minsize(500, 300)
 my_label = tkinter.Label(text="I am a label.", font=("Arial", 24, "bold"))
 my_label.pack()
 
-window.mainloop()
 
+input = tkinter.Entry(width=10)
+input.pack()
+
+
+def button_click_event():
+    print("I got clicked")
+    #my_label["text"] = "Button got clicked"
+    my_label["text"] = input.get()
+
+
+button = tkinter.Button(text="Click Me", command=button_click_event)
+button.pack()
+
+
+
+
+
+window.mainloop()
