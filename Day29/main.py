9a72aaee9854e9c3b5e89142c67c5320f9274a03
changhunJung <changhun611@gmail.com>
2022-11-02 00:39:27 +0900

    Search user/password from json formatted data

--- Day29/main.py
+++ Day29/main.py
@@ -4,7 +4,7 @@ import random
 import pyperclip
 import json
 
-WIDTH=45
+WIDTH=48
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -55,12 +55,45 @@ def save_password():
             }
         }
 
-        json_file = open("data.json", "w")
-        json.dump(new_data, json_file)
-        json_file.close()
-        website_entry.delete(0, "end")
-        password_entry.delete(0, "end")
+        try:
+            with open("data.json", "r") as json_file:
+                json_data = json.load(json_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as json_file:
+                json.dump(new_data, json_file, indent=4)
+        else:
+            with open("data.json", "w") as json_file:
+                json_data.update(new_data)
+                json.dump(json_data, json_file, indent=4)
+        finally:
+            website_entry.delete(0, "end")
+            password_entry.delete(0, "end")
 
+
+# ----------------------- Search Password ----------------------------- #
+def search_password():
+    try:
+        with open("data.json", "r") as json_file:
+            json_data = json.load(json_file)
+            website = website_entry.get()
+            if len(website) == 0:
+                messagebox.showwarnning(title="Oops", message="Sorry 'website' field is empty.")
+                print("Sorry 'website' field is empty.")
+                return
+            username = json_data[website]["user"]
+            password = json_data[website]["password"]
+    except FileNotFoundError:
+        messagebox.showwarning(title="Data File not Found",
+                               message=f"Sorry there's no information in {website}.")
+        print(f"Sorry there's no information in {website}.")
+    except KeyError:
+        messagebox.showwarning(title="Website not Found",
+                               message=f"Sorry there's no information in {website}.")
+        print(f"Sorry there's no information in {website}.")
+    else:
+        messagebox.showinfo(title=website, message=f"user: {username}\npassword: {password}")
+    finally:
+        website_entry.delete(0, "end")
 
 # ---------------------------- UI SETUP ------------------------------- #
 #window = tkinter.Tk(width=240, height=240, title="Password manager")
@@ -75,9 +108,12 @@ canvas.grid(row=0, column=1)
 
 website_label = tkinter.Label(text="Website:")
 website_label.grid(row=1, column=0)
-website_entry = tkinter.Entry(width=WIDTH)
+website_entry = tkinter.Entry(width=30)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+# website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
+website_search_button = tkinter.Button(text="Search", width=16, command=search_password)
+website_search_button.grid(row=1, column=2)
 
 username_label = tkinter.Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
@@ -87,7 +123,7 @@ username_entry.insert(0, "changhun611")
 
 password_label = tkinter.Label(text="Password:")
 password_label.grid(row=3, column=0)
-password_entry = tkinter.Entry(width=28)
+password_entry = tkinter.Entry(width=30)
 password_entry.grid(row=3, column=1)
 password_button = tkinter.Button(text="GeneratePassword", font=("Arerial, 8"), command=create_password)
 password_button.grid(row=3, column=2)
