23b7bab05552ead4dae5860d58d77b50cdf4b679
changhunJung <changhun611@gmail.com>
2022-11-06 19:47:03 +0900

    Build a GUI Quiz program. Use OOP concept. Use requests.get() API Use tkinter GUI.

---
+++ Day34/main.py
@@ -0,0 +1,10 @@
+from question_model import Question
+#from data import question_data
+from quiz_brain import QuizBrain
+import requests
+import tkinter
+from ui import GraphicUserInterface
+
+
+quiz = QuizBrain()
+gui = GraphicUserInterface(quiz)
