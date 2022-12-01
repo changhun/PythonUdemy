f6e0d32024e7b11c07f4cdfe57c196ad5be99c28
changhunJung <changhun611@gmail.com>
2022-12-01 23:53:41 +0900

    Use BeautifulSoup selector

--- Day45/main.py
+++ Day45/main.py
@@ -1,7 +1,41 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", encoding="utf-8") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
 
-soup = BeautifulSoup(contents, 'html.parser')
-print(soup.title.string)
+soup = BeautifulSoup(yc_web_page, "html.parser")
+#print(soup)
+titleline = soup.select(".titleline")
+for tag in titleline:
+    #print(tag.getText())
+    print(tag.text)
+
+
+#print(soup.select_one(".titleline").text)
+
+# with open("website.html", encoding="utf-8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, 'html.parser')
+# # print(soup.title.string)
+# # print(soup.prettify())
+# # print(soup.p)
+#
+# all_anchor_tags = soup.find_all(name="a")
+# #print(all_anchor_tags)
+# for tag in all_anchor_tags:
+#     # print(tag)
+#     # print(tag.getText())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(soup.find(name="h1", id="name").string)
+# print(soup.find(name="h1", id="name").getText())
+#
+#
+# print(soup.select_one("p a"))
+# print(soup.select_one("#name"))
+#
+# print(soup.select(selector=".heading"))
+# print(soup.select(".heading"))
