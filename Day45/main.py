5c8496fad8d83a0601506fb26afe4d29c1b5612b
changhunJung <changhun611@gmail.com>
2022-12-03 23:16:44 +0900

    write list to file using slice

--- Day45/main.py
+++ Day45/main.py
@@ -5,14 +5,25 @@ response = requests.get("https://news.ycombinator.com/")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-#print(soup)
-titleline = soup.select(".titleline")
-for tag in titleline:
-    #print(tag.getText())
-    print(tag.text)
+# titleline = soup.select(".titleline")
+# for tag in titleline:
+#     #print(tag.getText())
+#     print(tag.text)
+
+#print(soup.select_one(".titleline").select_one('a'))
+#print(soup.select_one(".titleline").a)
+
+article_tag = soup.select_one(".titleline").a
+article_text = soup.select_one(".titleline").a.text
+article_link = soup.select_one(".titleline").a.get("href")
+#print(article_link)
+article_upvote = soup.select_one(".score").getText().split()[0]
+print(article_upvote)
 
 
-#print(soup.select_one(".titleline").text)
+
+
+
 
 # with open("website.html", encoding="utf-8") as file:
 #     contents = file.read()
