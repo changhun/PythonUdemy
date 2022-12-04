4575688ef249cf752e18d0e5e47bb73da755901c
changhunJung <changhun611@gmail.com>
2022-12-04 23:36:46 +0900

    get 100 best musices

--- Day46/main.py
+++ Day46/main.py
@@ -1,23 +1,24 @@
+import TOKEN
 from bs4 import BeautifulSoup
 import requests
 
 
-#URL = "https://www.billboard.com/charts/hot-100/"
-URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
-
-#date = input("Which date you want to go back? (Type yyyy-mm-dd): ")
-#URL = URL + date + "/"
-#print(URL)
+#URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
+URL = "https://www.billboard.com/charts/hot-100/"
+date = input("Which date you want to go back? (Type yyyy-mm-dd): ")
+URL = URL + date + "/"
 
 response = requests.get(URL)
 content = response.text
 soup = BeautifulSoup(content, "html.parser")
 #title_tags = soup.find_all(name="h3", class_="c-title")
 title_tags = soup.select("li h3")
-# for tag in title_tags:
-#    print(tag)
-titles = [tag.getText() for tag in title_tags]
-for title in titles:
-    print(title.strip())
-#print(titles)
 
+titles = [tag.getText() for tag in title_tags[0:100]]
+i = 0
+for title in titles:
+    print(i, title.strip())
+    i += 1
+
+
+print(TOKEN.CLIENT_ID)
